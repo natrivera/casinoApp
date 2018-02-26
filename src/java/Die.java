3bc28bff50271ea9fe678cd6ee98099f3196be17
inductiveftw <blink182isnotemo@gmail.com>
2018-02-25 17:39:55 -0800

    Added some Craps functionaltiy

--- src/java/Die.java
+++ src/java/Die.java
@@ -3,16 +3,25 @@ import java.security.SecureRandom;
 public class Die {
     private int value;
     private SecureRandom randomNumbers = new SecureRandom();
+    private String imageURL;
     
     public Die(){
         
     }
     
+    //gets a random number from 1 to 6
     public void roll(){
         value = 1 + randomNumbers.nextInt(6);
+        imageURL = "img/" + value + ".jpg";
     }
     
+    //returns rolled value
     public int getValue(){
         return value;
+    }
+    
+    //returns image
+    public String getImageURL(){
+        return imageURL;
     }
 }
