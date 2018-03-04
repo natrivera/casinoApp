f114e66b7a3edc7ba42c367c4527a21805f5fd12
natrivera <nat@natrivera.com>
2018-03-03 20:00:33 -0800

    sql scripts

---
+++ sql_scripts/game_t_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO game_t (GameID , Name , Description)
+   VALUES (1 , "Poker" , "Five card stud" ) , 
+		  (2 , "Craps" , "Not sure what this is" ) ,
+          (3 , "Hi-lo" , "Highest card wins") ,
+          (4 , "Bingo" , "Bingo");
+
+
---
+++ sql_scripts/user_t_insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO  user_t (UserID , UserName , DateOfBirth , Password , Name , DateCreated , Balance)
+   VALUES (1 , "Pattycakes" , "1999-01-01" , "sesame" , "Patty Cakes" , "2017-10-07" , 1345 ) , 
+		  (2 , "Jesriv317" , "1999-01-01" , "sesame" , "Jesus Name" , "2017-10-07" , 989 ) ,
+          (3 , "Natrivera" , "1999-01-01" , "sesame" , "Nat Rivera" , "2017-10-07" , 2332 ) ,
+          (4 , "Poly1123" , "1999-01-01" , "sesame" , "Cal Poly" , "2017-10-07" , 5432 ) ,
+          (5 , "Cis424" , "1999-01-01" , "sesame" , "Comp Info" , "2017-10-07" , 1000 ) ,
+          (6 , "Dodgersfan" , "1999-01-01" , "sesame" , "Dodgers Fan" , "2017-10-07" , 8778 ) ,
+          (7 , "Lakersbeast" , "1999-01-01" , "sesame" , "Lakers Fan" , "2017-10-07" , 132 ) ,
+          (8 , "Weirdbeast" , "1999-01-01" , "sesame" , "James Brown" , "2017-10-07" , 324 ) ,
+          (9 , "Onthemedia" , "1999-01-01" , "sesame" , "Sally Fields" , "2017-10-07" , 425 ) ,
+          (10 , "Planetmoney" , "1999-01-01" , "sesame" , "Will Ferrell" , "2017-10-07" , 532 ) ,
+          (11 , "Thisamericanlife" , "1999-01-01" , "sesame" , "Ira Glass" , "2017-10-07" , 1345 ) ,
+          (12 , "Reveal" , "1999-01-01" , "sesame" , "John Relily" , "2017-10-07" , 3242 ) ,
+          (13 , "User" , "1999-01-01" , "sesame" , "Test User" , "2017-10-07" , 342 ) , 
+          (14 , "Test" , "1999-01-01" , "sesame" , "User test" , "2017-10-07" , 553 );
+ 
